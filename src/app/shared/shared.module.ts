166721de74f10972119b835c5d446808fcc9e479
Hector Lovo <lovohh@gmail.com>
2018-01-04 18:26:21 -0500

    Adding AppRouting
    Command-line:
    - 'ng g module app-routing'

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   imports: [
+    AppRoutingModule,
     CommonModule,
     FormsModule,
     NgbModule.forRoot()
   ],
   declarations: [],
   exports: [
+    AppRoutingModule,
     CommonModule,
     FormsModule,
     NgbModule
