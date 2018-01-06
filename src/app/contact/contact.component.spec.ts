4cb98e59471b7521d20f1b4b11302b6bd48d7c18
Hector Lovo <lovohh@gmail.com>
2018-01-06 11:03:08 -0500

    Adding FlipcardComponent To The 'contact' Page

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
+import { FlipcardComponent } from '../shared';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -8,7 +9,7 @@ describe('ContactComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ ContactComponent, FlipcardComponent ]
     })
     .compileComponents();
   }));
