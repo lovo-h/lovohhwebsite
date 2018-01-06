b92c645000f351a76900323f270f6340b2cad4b4
Hector Lovo <lovohh@gmail.com>
2018-01-05 22:14:39 -0500

    Fixing RouterLink Issue In FooterComponent
    Issue:
    - The footer was trying to use the 'routerLink' in the tests,
      without first importing the 'AppRoutingModule', contained
      in the 'SharedModule'
    
    Fix:
    - Imported 'SharedModule' into FooterComponent's tests
    - Also, imported all the required modules for the
      'AppRoutingModule' to navigate.

--- src/app/shared/layout/footer/footer.component.spec.ts
+++ src/app/shared/layout/footer/footer.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { SharedModule } from '../../shared.module';
 import { FooterComponent } from './footer.component';
+
+import { HomeComponent } from '../../../home/home.component';
+import { ResumeComponent } from '../../../resume/resume.component';
+import { ProjectsComponent } from '../../../projects/projects.component';
+import { BlogComponent } from '../../../blog/blog.component';
+import { ContactComponent } from '../../../contact/contact.component';
+import { Status404Component } from '../../../status404/status404.component';
+
+import { APP_BASE_HREF } from '@angular/common';
+
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,7 +19,17 @@ describe('FooterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [
+        FooterComponent,
+        HomeComponent,
+        ResumeComponent,
+        ProjectsComponent,
+        BlogComponent,
+        ContactComponent,
+        Status404Component
+      ],
+      imports: [ SharedModule ],
+      providers: [{provide: APP_BASE_HREF, useValue : '/' }],
     })
     .compileComponents();
   }));
