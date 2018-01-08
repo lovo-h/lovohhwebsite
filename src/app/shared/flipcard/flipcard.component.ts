cc2f24c4452427b176101990fbaba6b43800547e
Hector Lovo <lovohh@gmail.com>
2018-01-07 21:30:09 -0500

    FlipCard: Adding Tilt Animation & Adjusting Shadow

--- src/app/shared/flipcard/flipcard.component.ts
+++ src/app/shared/flipcard/flipcard.component.ts
@@ -12,25 +12,55 @@ const CARD_FLIP_ANIMATIONS = [
   transition('back => front', animate('500ms ease-in'))
 ];
 
+const CARD_TILT_ANIMATIONS = [
+  state('untilted-front', style({
+    transform: 'rotateY(0)'
+  })),
+  state('tilted-front', style({
+    transform: 'rotateY(10.0deg)'
+  })),
+  state('untilted-back', style({
+    transform: 'rotateY(179.9deg)'
+  })),
+  state('tilted-back', style({
+    transform: 'rotateY(169.9deg)'
+  })),
+  transition('untilted-front => tilted-front', animate('500ms ease-out')),
+  transition('tilted-front => untilted-front', animate('500ms ease-in')),
+  transition('tilted-back => untilted-back', animate('500ms ease-in')),
+  transition('tilted-back => untilted-back', animate('500ms ease-in'))
+];
+
 @Component({
   selector: 'app-flipcard',
   templateUrl: './flipcard.component.html',
   styleUrls: ['./flipcard.component.scss'],
   animations: [
-    trigger('flipState', CARD_FLIP_ANIMATIONS)
+    trigger('flipState', CARD_FLIP_ANIMATIONS),
+    trigger('tiltState', CARD_TILT_ANIMATIONS)
   ]
 })
 export class FlipcardComponent implements OnInit {
   cardSide: string;
+  cardTilt: string;
 
   constructor() {
   }
 
   ngOnInit() {
     this.cardSide = 'front';
+    this.cardTilt = 'untilted-front';
   }
 
   toggleFlip() {
     this.cardSide = (this.cardSide === 'back') ? 'front' : 'back';
+  }
+
+  mouseEnter() {
+    this.cardTilt = 'tilted-' + this.cardSide;
+  }
+
+  mouseLeave() {
+    this.cardTilt = 'untilted-' + this.cardSide;
   }
 }
